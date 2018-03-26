5c819b146166da9547543dfd520edf01a27f6859
jammmes <glazov.it@gmail.com>
2018-03-26 20:58:12 +0400

    refactoring

--- lesson6/main.js
+++ lesson6/main.js
@@ -1,31 +1,31 @@
 (function ($) {
   $(function () {
-        // Элементы формы
+    // Элементы формы
     var btn = $('#sendButton')
     var userName = $('#userName')
     var phone = $('#phone')
     var email = $('#email')
     var comment = $('#comment')
     var birthDay = $('#birthDay')
-    // Регулярные выражения для проверки
+      // Регулярные выражения для проверки
     var pcreName = /([^\d\s])+/
     var pcrePhone = /^\+7\(\d{3}\)\d{3}-\d{4}$/
     var pcreEmail = /^[a-zA-Z0-9]+[a-zA-Z0-9.-]*[a-zA-Z0-9]@[a-zA-Z0-9]+[a-zA-Z0-9.-]+[a-zA-Z0-9]$/
     var pcreComment = /.+/
     var pcreBirthDay = /^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/
 
-        /** Проверяем на валидность элементы и отправляем данные для
-         *  визуализации уведомления
-         */
-    function checkRegexp () {
-            //
+    /** Проверяем на валидность элементы и отправляем данные для
+     *  визуализации уведомления
+     */
+    function checkRegexp() {
+      //
       var warningArr = []
       var textName = userName.eq(0).val()
       var textBirthDay = birthDay.eq(0).val()
       var phoneNumber = phone.eq(0).val()
       var emailText = email.eq(0).val()
       var commentText = comment.eq(0).val()
-            // Проверяем Имя
+        // Проверяем Имя
       if ((!textName) || (!pcreName.test(textName))) {
         warningArr.push(userName)
       }
@@ -51,25 +51,25 @@
       }
     }
 
-        /** Выводим уведомление о неправильном формате
-         *
-         *
-         * @param {any} elements - массив элементов для уведомления
-         */
-    function showWarning (elements) {
-            // Удалим старое окно
+    /** Выводим уведомление о неправильном формате
+     *
+     *
+     * @param {any} elements - массив элементов для уведомления
+     */
+    function showWarning(elements) {
+      // Удалим старое окно
       $('.ui-dialog').remove()
-            //
+        //
       var textDialog = ''
       var i = 1
       elements.forEach(function (element) {
-                // Сделаем анимацию на инпут
-        element.effect('bounce', 500)
-                // Соберем текст сообщения для всех элементов
-        textDialog += ' \n' + i + '.) ' + $(element).parent().find('.dialogText').children(0).text()
-        i++
-      })
-            // Выводим диалог
+          // Сделаем анимацию на инпут
+          element.effect('bounce', 500)
+            // Соберем текст сообщения для всех элементов
+          textDialog += ' \n' + i + '.) ' + $(element).parent().find('.dialogText').children(0).text()
+          i++
+        })
+        // Выводим диалог
       $('<div/>', {
         text: textDialog
       }).dialog({
@@ -99,12 +99,12 @@
       checkRegexp()
     })
 
-        // ********************- Задание№ 3 и 4 -******************************
+    // ********************- Задание№ 3 и 4 -******************************
 
-        // Инициируем карусель
+    // Инициируем карусель
     $(function () {
       $('.jcarousel').jcarousel({
-                // Configuration goes here
+        // Configuration goes here
         animation: {
           duration: 900,
           easing: 'linear',
@@ -120,7 +120,7 @@
       autostart: true
     })
 
-        // Настраиваем перетаскивание из карусели в карзину
+    // Настраиваем перетаскивание из карусели в карзину
     $('.carousel__item').draggable({ opacity: 0.7, helper: 'clone' })
     $('#cartBlock').droppable({
       drop: function (event, ui) {
@@ -129,11 +129,11 @@
           class: 'cartList__item'
         }))
         item.append($('<div/>', {
-          text: ui['draggable'].find('.goodTitle').eq(0).prop('textContent'),
+          text: ui['draggable'].find('.goodTitle').eq(0).text(),
           class: 'goodName'
         }))
         item.append($('<div/>', {
-          text: ui['draggable'].find('.goodPrice').eq(0).prop('textContent'),
+          text: ui['draggable'].find('.goodPrice').eq(0).text(),
           class: 'goodPrice'
         }))
         $('#cartList').append(item)
