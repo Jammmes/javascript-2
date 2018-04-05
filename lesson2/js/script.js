b13ce4a9e7d25c5d712789c0c157272cc45f76d3
Eugen <glazov.it@gmail.com>
2018-04-05 14:12:46 +0400

    separated rendering from loading

--- lesson2/js/script.js
+++ lesson2/js/script.js
@@ -10,66 +10,78 @@ var pokemonCount = 0;
  * @param URL
  */
 function PokemonStorage(url) {
-  this.url = url;
-  this.arrPokemons = [];
-  this.lastOffset = 0;
+    this.url = url;
+    this.arrPokemons = [];
+    this.lastOffset = 0;
 }
 
-/* Загрузить покемонов с URL, записать в хранилище и отрисовать на странице
- *@return NodeList с покемонами в функцию doPokemonsClick
+
+/** Отрисовка дом элементов на основании полученных данных
+ * 
+ * 
+ * @param {any} loadResults - результат AJAX запроса
  */
-PokemonStorage.prototype.loadPokemons = function () {
-  var pokemons;
-  // сохраним контекст хранилища
-  var context = this;
-  //
-  var xhr = new XMLHttpRequest();
-  var url = this.url;
-  xhr.open('GET', url, true);
-  xhr.timeout = TIME_OUT;
-  xhr.ontimeout = function () {
-    console.log('Время вышло');
+PokemonStorage.prototype.renderPokemons = function(loadResults) {
+    //
     var container = document.getElementById('container');
-    container.innerHTML = '<h1 class="error"> Pokemons do not loaded, please press F5 for reload the page</>';
-  }
-  xhr.send();
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      if (xhr.status === 200) {
-        var loadResults = JSON.parse(xhr.responseText);
-        //Начинаем отрисовку DOM элементов
-        var container = document.getElementById('container');
-        var row = document.createElement('div');
+    var row = document.createElement('div');
+    //
+    for (var i = 0; i < loadResults['results'].length; i++) {
+        var pokemonName = loadResults['results'][i]['name'];
+        var item = document.createElement('div');
+        var title = document.createElement('div');
+        var img = document.createElement('img');
         //
-        for (var i = 0; i < loadResults['results'].length; i++) {
-          var pokemonName = loadResults['results'][i]['name'];
-          var item = document.createElement('div');
-          var title = document.createElement('div');
-          var img = document.createElement('img');
-          //
-          item.classList.add('item');
-          title.classList.add('item__title');
-          img.classList.add('item__img');
-          //
-          title.innerText = pokemonName;
-          img.src = 'img/' + (pokemonCount + 1) + '.png';
-          //
-          item.appendChild(title);
-          item.appendChild(img);
-          row.appendChild(item);
-          //Запишем данные покемона в хранилище
-          context.arrPokemons.push({
+        item.classList.add('item');
+        title.classList.add('item__title');
+        img.classList.add('item__img');
+        //
+        title.innerText = pokemonName;
+        img.src = 'img/' + (pokemonCount + 1) + '.png';
+        //
+        item.appendChild(title);
+        item.appendChild(img);
+        row.appendChild(item);
+        //Запишем данные покемона в хранилище
+        this.arrPokemons.push({
             id: pokemonCount,
-            url: loadResults['results'][i]['url'],
-            name: loadResults['results'][i]['name']
-          });
-          pokemonCount++;
-        }
-        row.classList.add('row');
-        container.appendChild(row);
-        pokemons = document.querySelectorAll(".item");
-        doPokemonsClick(pokemons);
-      }
+            url: loadResults['results'][i].url,
+            name: loadResults['results'][i].name
+        });
+        pokemonCount++;
     }
-  }
+    row.classList.add('row');
+    container.appendChild(row);
+    pokemons = document.querySelectorAll(".item");
+    doPokemonsClick(pokemons);
+}
+
+/**
+ * 
+ * 
+ */
+PokemonStorage.prototype.loadPokemons = function() {
+    var pokemons;
+    // сохраним контекст хранилища
+    var context = this;
+    //
+    var xhr = new XMLHttpRequest();
+    var url = this.url;
+    xhr.open('GET', url, true);
+    xhr.timeout = TIME_OUT;
+    xhr.ontimeout = function() {
+        console.log('Время вышло');
+        var container = document.getElementById('container');
+        container.innerHTML = '<h1 class="error"> Pokemons do not loaded, please press F5 for reload the page</>';
+    }
+    xhr.send();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                var loadResults = JSON.parse(xhr.responseText);
+                //Передать результаты на отрисовку DOM элементов
+                context.renderPokemons(loadResults);
+            }
+        }
+    }
 }
